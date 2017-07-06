6cb23a981f17a87a9379785173e2a12be856a04e
admin <admin@qaplanet38>
2017-07-06 10:25:36 +0530

    New Java Project

---
+++ Project/src/ClassA.java
@@ -0,0 +1,30 @@
+
+public class ClassA {
+
+	
+	public void method()
+	{
+		System.out.println("Method");
+	}
+	
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+	
+	public void method3()
+	{
+		System.out.println("Method3");
+	}
+	public static void main(String[] args) 
+	{
+	
+		ClassA obj=new ClassA();
+		
+		obj.method();
+		obj.method2();
+		obj.method3();
+
+	}
+
+}
